ffc8c636e0601120bd727679084b78ad4cffd4f6
MohamedGEDI <gedimohammed@gmail.com>
2021-09-13 09:20:23 +0000

    replace

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    n = len(my_list)
+    if idx < 0 or idx > n - 1:
+        return my_list
+    else:
+        replace = my_list
+        replace[idx] = element
+        return replace
