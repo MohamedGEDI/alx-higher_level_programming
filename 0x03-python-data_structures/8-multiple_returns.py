9dd507294043d784b662c1e043333c7f060518b7
MohamedGEDI <gedimohammed@gmail.com>
2021-09-13 14:59:02 +0000

    ruple return

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/us/bin/python3
+def multiple_returns(sentence):
+    l = len(sentence)
+    if l == 0:
+        first_letter = None
+    else:
+        first_letter = sentence[0]
+    tuple_r = (l, first_letter)
+    return tuple_r
