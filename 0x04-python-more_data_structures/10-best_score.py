9b7c1039873773416f1b40ea0974cc1f24750d48
MohamedGEDI <gedimohammed@gmail.com>
2021-09-15 17:35:40 +0000

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    else:
+        temp = 0
+        for k, v in a_dictionary.items():
+            if temp <= v:
+                temp = v
+        key_list = list(a_dictionary.keys())
+        val_list = list(a_dictionary.values())
+        posititon = val_list.index(temp)
+        return key_list[position]
