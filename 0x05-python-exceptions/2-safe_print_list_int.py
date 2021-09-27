c88b4476f12be3a701e2205ab828d4b37760dd77
MohamedGEDI <gedimohammed@gmail.com>
2021-09-27 15:23:36 +0000

    print listint

---
+++ 0x05-python-exceptions/2-safe_print_list_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_int(mylist=[], x=0):
+    count = 0
+    for i in my_list:
+        count = count + 1
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            test = test + 1
+        except:
+            pass
+    print()
+    return test
+          
