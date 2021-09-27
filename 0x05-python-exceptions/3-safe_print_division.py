b96ef4add90ba79cb0de00a330dc44bcde043fd6
MohamedGEDI <gedimohammed@gmail.com>
2021-09-27 15:57:02 +0000

    0 / error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    result = 0
     try:
         result = a / b
-        return result
     except ZeroDivisionError:
         result = "None"
-        return result
     finally:
         print("Inside finally: {}".format(result))
+    return result
