079aa08d770b8a512a8ceee1093da116c4943b54
MohamedGEDI <gedimohammed@gmail.com>
2021-09-27 15:08:16 +0000

    zero division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,9 @@ def safe_print_division(a, b):
     result = 0
     try:
         result = a / b
+        return result
     except:
         result = "None"
+        return result
     finally:
         print("Inside finally: {}".format(result))
