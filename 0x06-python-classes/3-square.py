22457bbe847987c64796bb6abf9d0cc91a3cd73f
MohamedGEDI <gedimohammed@gmail.com>
2021-09-28 16:33:15 +0000

    area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+"""simple square class"""
+
+
+class Square:
+    """initiated class"""
+    def __init__(self, size=0):
+        if (not isinstance(size, int)):
+            raise TypeError("size must be integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+       result = self.__size * self.__size
+       return result
