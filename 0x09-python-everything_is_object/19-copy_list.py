17ce0c92c789b2fddf439c24a483c6b38074f064
MohamedGEDI <gedimohammed@gmail.com>
2021-10-05 19:18:49 +0000

    copy list

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-"""Function to copy list"""
 def copy_list(my_list):
-    """copy list
-       args:
-            list - anytype
-       result:
-            new list
-    """
     new_list = my_list[:]
     return new_list
