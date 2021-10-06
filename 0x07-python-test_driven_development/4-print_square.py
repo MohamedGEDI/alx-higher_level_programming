bf5d19a74293935561a69bce96321995987c4128
MohamedGEDI <gedimohammed@gmail.com>
2021-10-06 07:34:05 +0000

    shebang

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 """print #in square format """
+
+
 def print_square(size):
+    """function to print # in a square fashion
+    arg:
+       size of square
+    result:
+       size shaped square
+    """
     if type(size) is not int:
         raise TypeError("size must be an integer")
     elif size < 0:
