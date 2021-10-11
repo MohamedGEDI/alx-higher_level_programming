7c7cec739b990a3965870aedbb6b56e9b46e1335
MohamedGEDI <gedimohammed@gmail.com>
2021-10-11 19:38:59 +0000

    validate sides

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,9 +2,13 @@
 """inherit base geometry"""
 
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """functions on rectangle"""
     def __init__(self, width, height):
+        """validate and initiate class"""
         super().integer_validator("width", width)
         super().integer_validator("height", height)
         self.__width = width
