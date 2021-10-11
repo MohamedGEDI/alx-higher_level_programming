391d73842faa1ce323b7ddb2e565fee8232c89a4
MohamedGEDI <gedimohammed@gmail.com>
2021-10-11 14:49:53 +0000

    isinstance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""check if is sclass"""
+
+
+def is_same_class(obj, a_class):
+    """check if is instance of """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
