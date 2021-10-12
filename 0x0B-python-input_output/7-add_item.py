c92dc7fb30c38a85790ea37f24806b0d7a02b872
MohamedGEDI <gedimohammed@gmail.com>
2021-10-12 15:50:06 +0000

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,9 +2,9 @@
 """import all files and modules"""
 
 
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-import sys
 
 
 alist = list()
