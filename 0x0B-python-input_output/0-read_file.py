7fb12443c2e43744520f4e08971b833bfb6e69c7
MohamedGEDI <gedimohammed@gmail.com>
2021-10-12 09:24:48 +0000

    write utf-8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """create functin to read file """
 def read_file(filename=""):
-    with open(filename) as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read())
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""write file"""
+
+
+def write_file(filename="", text=""):
+    """function to write a file"""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        f.write(text)
