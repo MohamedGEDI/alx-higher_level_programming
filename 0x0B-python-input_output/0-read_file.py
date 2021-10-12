8b3cdd20d43b1e4c5751b167b5c97b748b01116f
MohamedGEDI <gedimohammed@gmail.com>
2021-10-12 13:21:19 +0000

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,5 +2,5 @@
 """create functin to read file """
 def read_file(filename=""):
     """read file and print to stdout"""
-    with open(filename, encoding="utf-8") as f:
+    with open(filename) as f:
         print(f.read())
