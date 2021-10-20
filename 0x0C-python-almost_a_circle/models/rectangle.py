5fb3ef3e50f1be80ea65ccad22f8c449cf0deb44
MohamedGEDI <gedimohammed@gmail.com>
2021-10-20 09:03:25 +0000

    fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""Class rectangle inherits from base class"""
-from models.base import Base
+
+
+from .base import Base
+"""this is an rectangle class"""
+
 
 class Rectangle(Base):
-    """Put super of base class init"""
+    """rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ inherit super """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
- 
-     
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
     @property
     def width(self):
         return self.__width
@@ -20,7 +21,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         if type(width) is not int:
-            raise TypeError("width must be an int")
+            raise TypeError("width must be an integer")
         elif width <= 0:
             raise ValueError("width must be > 0")
         else:
@@ -33,7 +34,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, height):
         if type(height) is not int:
-            raise TypeError("height must be an int")
+            raise TypeError("height must be an integer")
         elif height <= 0:
             raise ValueError("height must be > 0")
         else:
@@ -46,9 +47,9 @@ class Rectangle(Base):
     @x.setter
     def x(self, x):
         if type(x) is not int:
-            raise TypeError("x must be an int")
+            raise TypeError("x must be an integer")
         elif x < 0:
-            raise ValueError("x must be > 0")
+            raise ValueError("x must be >= 0")
         else:
             self.__x = x
 
@@ -59,35 +60,11 @@ class Rectangle(Base):
     @y.setter
     def y(self, y):
         if type(y) is not int:
-            raise TypeError("y be an int")
+            raise TypeError("y must be an integer")
         elif y < 0:
-            raise ValueError("y must be > 0")
+            raise ValueError("y must be >= 0")
         else:
             self.__y = y
 
-        def update(self, *args, **kwargs):
-            i = 0
-            attributes = ['id', 'width', 'height', 'x', 'y']
-            if len(args) > 0:
-                for attr in attributes:
-                    if i > len(args) - 1:
-                        break
-                    setattr(self, attr, args[i])
-            else:
-                for key, value in kwargs.items():
-                    if key not in attributes:
-                        continue
-                    setattr(self, key, value)
-                        
-
-        def area(self):
-            return self.__width * self.__height
-
-        def display(self):
-            for i in range(self.__y):
-                print()
-            for i in range(self.__height):
-                print(" " * self.__x  + "#" * self.__width)
-
-        def __str__(self):
-            return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+    def area(self):
+        return self.__height * self.__width
