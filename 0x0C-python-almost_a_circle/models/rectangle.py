19f592846bef19cbcc2ca2edc2100f5255d77ab5
MohamedGEDI <gedimohammed@gmail.com>
2021-10-20 08:22:58 +0000

    fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,57 +13,57 @@ class Rectangle(Base):
         self.__y = y
  
      
-        @property
-        def width(self):
-            return self.__width
+    @property
+    def width(self):
+        return self.__width
 
-        @width.setter
-        def width(self, width):
-            if type(width) is not int:
-                raise TypeError("width must be an int")
-            elif width <= 0:
-                raise ValueError("width must be > 0")
-            else:
-                self.__width = width
-        
-        @property
-        def height(self):
-            return self.__height
+    @width.setter
+    def width(self, width):
+        if type(width) is not int:
+            raise TypeError("width must be an int")
+        elif width <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = width
 
-        @height.setter
-        def height(self, height):
-            if type(height) is not int:
-                raise TypeError("height must be an int")
-            elif height <= 0:
-                raise ValueError("height must be > 0")
-            else:
-                self.__height = height
+    @property
+    def height(self):
+        return self.__height
 
-        @property
-        def x(self):
-            return self.__x
+    @height.setter
+    def height(self, height):
+        if type(height) is not int:
+            raise TypeError("height must be an int")
+        elif height <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = height
 
-        @x.setter
-        def x(self, x):
-            if type(x) is not int:
-                raise TypeError("x must be an int")
-            elif x < 0:
-                raise ValueError("x must be > 0")
-            else:
-                self.__x = x
+    @property
+    def x(self):
+        return self.__x
 
-        @property
-        def y(self):
-            return self.__y
+    @x.setter
+    def x(self, x):
+        if type(x) is not int:
+            raise TypeError("x must be an int")
+        elif x < 0:
+            raise ValueError("x must be > 0")
+        else:
+            self.__x = x
 
-        @y.setter
-        def y(self, y):
-            if type(y) is not int:
-                raise TypeError("y be an int")
-            elif y < 0:
-                raise ValueError("y must be > 0")
-            else:
-                self.__y = y
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        if type(y) is not int:
+            raise TypeError("y be an int")
+        elif y < 0:
+            raise ValueError("y must be > 0")
+        else:
+            self.__y = y
 
         def update(self, *args, **kwargs):
             i = 0
