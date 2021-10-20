ecc47fda43feda55523854e13277c6d39f45e906
MohamedGEDI <gedimohammed@gmail.com>
2021-10-20 09:27:50 +0000

    str

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,3 +85,6 @@ class Rectangle(Base):
         for i in range(self.__height):
             print("#" * self.__width)
 
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, sel
+f.__y, self.__width,self.__height)
