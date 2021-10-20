55775fb28b28ed66e1e0f69a843b5ef03ce1c17e
MohamedGEDI <gedimohammed@gmail.com>
2021-10-20 09:36:47 +0000

    disp

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,8 +82,9 @@ class Rectangle(Base):
 
     def display(self):
         """display the rectangle as # """
+        for i in range(self.__y)
         for i in range(self.__height):
-            print("#" * self.__width)
+            print(" "* self.__x + "#" * self.__width)
 
     def __str__(self):
         """string representation of Rectangle"""
