cc3cccd235ffe22a7cba58aba33f312dd66a0f6c
MohamedGEDI <gedimohammed@gmail.com>
2021-10-21 17:06:34 +0300

    square class created

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,16 +11,20 @@ from .rectangle import Rectangle
 class Square(Rectangle, Base):
     """class square that imports Rectangle and Base"""
     def __init__(self, size, x=0, y=0, id=None):
+        """Constructor imported from Rectangle"""
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
+        """size getter"""
         return self.width
 
     @size.setter
     def size(self, size):
+        """size setter"""
         self.width = size
         self.height = size
 
     def __str__(self):
+        "string representation of Square"
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
