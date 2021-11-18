b9e1adf24dc6768e8d600e99b1ea792d2b0fb72d
MohamedGEDI <gedimohammed@gmail.com>
2021-11-18 06:47:01 -0800

    genres

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- list by genres
 SELECT g.name AS genre, COUNT(tvg.genre_id) AS number_of_shows
-FROM tv_genres as g
+FROM tv_genres AS g
 INNER JOIN tv_show_genres tvg ON tvg.genre_id = g.id
 GROUP BY tvg.genre_id
 ORDER BY number_of_shows DESC
