1abe75ae88e88e7955c1fe3e121f6811308c86cf
MohamedGEDI <gedimohammed@gmail.com>
2021-11-18 07:04:29 -0800

    comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,8 @@
 -- list genres of dexter
-SELECT tvs.title FROM tv_shows
+SELECT tvs.title FROM tv_shows tvs
 INNER JOIN tv_show_genres tvg
-ON tvg.genre_id = tvs.id
-INNER JOIN tv_genre g
+ON tvg.show_id = tvs.id
+INNER JOIN tv_genres g
 ON tvg.genre_id = g.id
 WHERE g.name = 'Comedy'
 ORDER BY tvs.title ASC
