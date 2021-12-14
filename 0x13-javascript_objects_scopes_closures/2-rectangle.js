2a736268c9e0561650ab6a6a66eafe2ccfa40e4b
MohamedGEDI <gedimohammed@gmail.com>
2021-12-14 18:40:12 +0000

    def

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if ( w >= 1) {this.width = w;}
-    if ( h >= 1) {this.height = h;}
+    if ( w >= 1 && h >= 1) {
+      this.width = w;
+      this.height = h;}
   }
 }
 
