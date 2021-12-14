a8f210d4b1c07fe79f6b4f68161b80ef0572603b
MohamedGEDI <gedimohammed@gmail.com>
2021-12-14 14:23:52 +0000

    semi

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-if (process.argv[2])
-{
-let number = parseInt(process.argv[2]);
-if (isNaN(number) === false)
-{ console.log("My number: " + number);
-}
-else
-{ console.log("Not a number");
-}
-}
-else
-{ console.log("Not a number");
+if (process.argv[2]) {
+  const number = parseInt(process.argv[2]);
+  if (isNaN(number) === false) {
+    console.log('My number: ' + number);
+  } else {
+    console.log('Not a number');
+  }
+} else {
+  console.log('Not a number');
 }
