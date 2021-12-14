abdcfd705a5c10cd73a17504f5214ae38707d83a
MohamedGEDI <gedimohammed@gmail.com>
2021-12-14 15:11:50 +0000

    semi

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-let i = 0;
 let j = 0;
-let string = 'X';
+const string = 'X';
 if (process.argv[2]) {
   const number = parseInt(process.argv[2]);
   if (isNaN(number) === false) {
     for (; j < number; j++) {
       console.log(string.repeat(number));
-    } 
+    }
   } else {
     console.log('Missing size');
   }
