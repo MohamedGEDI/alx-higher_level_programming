264324a50051bc6d0fdee49ee754b6b33f37802b
MohamedGEDI <gedimohammed@gmail.com>
2021-12-14 19:34:11 +0000

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const oldSquare = require('./5-square.js');
+class Square extends oldSquare {
+  charPrint (c) {
+    let k = c;
+
+    if (!c) { k = 'X'; }
+
+    for (let i = 0; i < this.width; i++) {
+      console.log(k.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
