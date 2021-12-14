9713f3ed6ad7f2a0e9a11f4acd5b0e2ddbef8b21
MohamedGEDI <gedimohammed@gmail.com>
2021-12-14 14:45:05 +0000

    array

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+let language = 'C is fun';
+let i = 0
+if (process.argv[2])
+{ while (i < process.argv[2])
+  { console.log(language);
+    i += 1;
+  }
+}
+else { console.log("Missing number of occurences"); }
