335c06c59617c961cad7a16f1e64fa7dceda00ef
MohamedGEDI <gedimohammed@gmail.com>
2022-01-08 15:50:51 +0000

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,10 +8,10 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as responce:
-        html = responce.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
         print('''
-Body responce:
+Body response:
     - type: {}
-    - body: {}
-    - utf-8 content: 0K'''.format(type(html), html))
+    - content: {}
+    - utf8 content: 0K'''.format(type(html), html))
