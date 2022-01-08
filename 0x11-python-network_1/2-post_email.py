fa11e82c65061c77c314437087a56a392d81ede5
MohamedGEDI <gedimohammed@gmail.com>
2022-01-08 16:01:57 +0000

    add query to url

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+"""add email param to url"""
+
+
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    params = {"email": sys.argv[2]}
+    query = parse.urlencode(params)
+    with urllib.request.urlopen(sys.argv[1] + '?' + query) as responce:
+        head = responce.read().decode("utf-8")
+        print(head)
