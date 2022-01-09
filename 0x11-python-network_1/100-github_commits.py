8173b63a3178c07557e6a3b7b37c8dd61ff3d061
MohamedGEDI <gedimohammed@gmail.com>
2022-01-09 18:57:14 +0300

    use GitHub api to get commits and names

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+"""Use github PAT to get id"""
+
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    with requests.get(f'https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits',) as response:
+        if response.status_code == 200:
+            print(response.url)
+            r_dict = response.json()
+
+            for i in range(0, 10):
+                print("{}: {}".format(r_dict[i]['sha'], r_dict[i]['commit']['author']['name']))
+        else:
+            print("None")
