944dbdfd76c7baeca9a8b7142e849c2f6a87463d
MohamedGEDI <gedimohammed@gmail.com>
2022-01-09 19:00:56 +0300

    use GitHub api to get commits and names

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,7 +11,6 @@ import sys
 if __name__ == "__main__":
     with requests.get(f'https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits',) as response:
         if response.status_code == 200:
-            print(response.url)
             r_dict = response.json()
 
             for i in range(0, 10):
