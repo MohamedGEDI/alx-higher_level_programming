b9ee5f00a220b5cbb1d4109fc26c840f41a03824
MohamedGEDI <gedimohammed@gmail.com>
2022-01-09 08:28:07 +0000

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,6 +8,9 @@ import requests
 import sys
 
 
+"""get heade using request"""
+
+
 if __name__ == "__main__":
     with requests.get(sys.argv[1]) as responce:
         head = responce.headers['X-Request-Id']
