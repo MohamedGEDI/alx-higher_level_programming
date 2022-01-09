4c03cd4ea0936395e8d7cde9eda646688061abef
MohamedGEDI <gedimohammed@gmail.com>
2022-01-09 07:58:23 +0000

    check header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 
-"""get header corresponding to the one searched"""
+"""get header corresponding to the one searched usint requests"""
 
 
-import urllib.request
+import requests
 import sys
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as responce:
+    with requests.get(sys.argv[1]) as responce:
         head = responce.headers['X-Request-Id']
         print(head)
