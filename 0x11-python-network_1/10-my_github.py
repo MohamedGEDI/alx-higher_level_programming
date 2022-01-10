727c1a3a4eecbbbc815f59e0adc6b9007a7fbe34
MohamedGEDI <gedimohammed@gmail.com>
2022-01-10 10:59:59 +0300

    fix, no with

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,7 +11,8 @@ import sys
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    responce = requests.get('https://api.github.com/user', auth=(username, password))
+    responce = requests.get('https://api.github.com/user',
+                            auth=(username, password))
     if responce.status_code == 200:
         r_dict = responce.json()
         print(r_dict['id'])
