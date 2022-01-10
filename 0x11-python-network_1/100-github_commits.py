764b170643a4620bc8294d6660a345cae0ef52ff
MohamedGEDI <gedimohammed@gmail.com>
2022-01-10 11:08:57 +0300

    fix, no with

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,18 +2,27 @@
 
 
 """Use github PAT to get id"""
-
+import json
 
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    with requests.get(f'https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits',) as response:
-        if response.status_code == 200:
-            r_dict = response.json()
-
-            for i in range(0, 10):
-                print("{}: {}".format(r_dict[i]['sha'], r_dict[i]['commit']['author']['name']))
-        else:
-            print("None")
+    user = sys.argv[2]
+    repo = sys.argv[1]
+    headers = {
+        'Accept': 'application/vnd.github.v3+json',
+    }
+    params = {
+        'per_page': 10,
+    }
+    response = requests.get(f'https://api.github.com/repos/{user}/{repo}/'
+                            f'commits', headers=headers, params=params)
+    if response.status_code == 200:
+        r_dict = response.json()
+        for i in range(1, 10):
+            print("{}: {}".format(r_dict[i]['sha'],
+                                  r_dict['commit']['author']['name']))
+    else:
+        print("None")
