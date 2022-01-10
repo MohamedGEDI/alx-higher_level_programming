bd5573f9bd30ef4be25bf94150bf5bb49fee925e
MohamedGEDI <gedimohammed@gmail.com>
2022-01-10 10:24:30 +0300

    fix, no with

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,5 +11,5 @@ import sys
 if __name__ == "__main__":
     params = {"email": sys.argv[2]}
     responce = requests.post(sys.argv[1], params=params)
-    head = responce.content.decode("utf-8")
+    head = responce.text
     print(head)
